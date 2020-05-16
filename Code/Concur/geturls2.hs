df167ab255174484b003c10c9c186df99e6f6634
Marcin Benke <marcin.benke@gmail.com>
2020-05-16 10:39:58 +0200

    geturls2

---
+++ Code/Concur/geturls2.hs
@@ -0,0 +1,22 @@
+import GetURL
+
+import Control.Monad
+import Control.Concurrent
+data Async a = Async (MVar a)
+
+async :: IO a -> IO (Async a)
+async action = do
+   var <- newEmptyMVar
+   forkIO (action >>= putMVar var)
+   return (Async var)
+
+wait :: Async a -> IO a
+wait (Async var) = readMVar var
+
+main = do
+  m1 <- async $ getURL "http://evemaps.dotlan.net/map/Domain"
+  m2 <- async $ getURL "http://evemaps.dotlan.net/map/Lonetrek"
+  wait m1
+  putStrLn "1 DONE"
+  wait m2
+  putStrLn "2 DONE"
