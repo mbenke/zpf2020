acd7f64c2dbb06df29e46c0eac8de3487b3bcbf9
Marcin Benke <marcin.benke@gmail.com>
2020-05-16 13:02:52 +0200

    geturls3

--- Code/Concur/GetURL.hs
+++ Code/Concur/GetURL.hs
@@ -1,5 +1,12 @@
 module GetURL where
+import Data.ByteString(ByteString)
 import Network.HTTP
+import Network.URI(parseURI)
 
-getURL :: String -> IO String
-getURL url = simpleHTTP (getRequest url) >>= getResponseBody
+get urlString =
+  case parseURI urlString of
+    Nothing -> error ("getRequest: Not a valid URL - " ++ urlString)
+    Just u  -> mkRequest GET u
+
+getURL :: String -> IO ByteString
+getURL url = simpleHTTP (get url) >>= getResponseBody
